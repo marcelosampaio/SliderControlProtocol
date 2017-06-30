1508e3bed0ea657ee1e0d0a7f8a71608c3a5d925
Danilo Ricardo Dias Pena <danilord.pena@gmail.com>
2017-06-30 19:41:31 -0300

    Initial Commit

---
+++ Slider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Slider
+//
+//  Created by Marcelo on 6/30/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Slider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Slider
+//
+//  Created by Marcelo on 6/30/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
